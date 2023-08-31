7ed1380331a20d675bfa510797f2a3df551dcd6f
SumitGupta231 <sumit@revaalolabs.com>
2023-08-31 13:35:18 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [form, setform] = useState({})
+  const HandleForm = (e) => {
+    setform({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+  function HandleSubmit(e) {
+    e.preventDefault();
+    try{
+    axios.post('http://localhost:7000/demo', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      username:form.username,
+      password:form.password
+    })
+      .then((response) => {
+        console.log("response",response);
+      }, (error) => {
+        console.log("error",error);
+      });
+  }
+  catch(error){
+    console.log(error)
+  }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={HandleSubmit}>
+        <h1>{JSON.stringify(form)}</h1>
+        <span>username</span>
+        <input type="text" name="username" onChange={HandleForm}></input>
+        <span>password</span>
+        <input type="text" name="password" onChange={HandleForm}></input>
+        <input type="submit"></input>
+
+      </form>
+
     </div>
   );
 }
